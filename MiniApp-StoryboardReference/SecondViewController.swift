97d37f802b41f435afc900e270c23fb8cb6c47b9
mikekondo <foad5969@gmail.com>
2022-06-13 20:31:04 +0900

    SecondViewController編集

--- MiniApp-StoryboardReference/SecondViewController.swift
+++ MiniApp-StoryboardReference/SecondViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 final class SecondViewController: UIViewController {
 
     @IBOutlet private var countLabel: UILabel!
+    @IBOutlet private var countLabel2: UILabel!
     private var count: Int = 0
+    private var count2: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         countLabel.text = String(count)
+        countLabel2.text = String(count2)
     }
 
     @IBAction func didTapCountUpButton(_ sender: Any) {
         count += 1
+        count2 = count*2
         countLabel.text = String(count)
+        countLabel2.text = String(count2)
     }
 }
